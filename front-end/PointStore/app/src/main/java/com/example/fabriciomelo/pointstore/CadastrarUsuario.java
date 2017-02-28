6dab6cb2f286dcf3cca6edde453ae59b8ed3c0d7
Fabrício Melo <Fabrcio Melo>
2017-02-28 17:26:00 -0300

    Adicionando telas de login e cadastrar usuario

---
+++ front-end/PointStore/app/src/main/java/com/example/fabriciomelo/pointstore/CadastrarUsuario.java
@@ -0,0 +1,13 @@
+package com.example.fabriciomelo.pointstore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastrarUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_usuario);
+    }
+}
---
+++ front-end/PointStore/app/src/main/java/com/example/fabriciomelo/pointstore/Login.java
@@ -0,0 +1,26 @@
+package com.example.fabriciomelo.pointstore;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void logar(View v) {
+
+    }
+    public void cadastrarUsuario (View v) {
+        Intent cadastrarUsuario = new Intent(this, CadastrarUsuario.class);
+        startActivity(cadastrarUsuario);
+
+    }
+
+}
