b75bb7198328ef1546d4108a22fb1e0872b4ea42
Juanvic <juanvic.almeida@gmail.com>
2017-03-17 18:05:52 -0300

    Atualizando a Tela de Login
    Tela com a validação dos campos corrigida e cores dos botões com a cor
    certa

--- front-end/PointStore/app/src/main/java/br/com/pointstore/util/Login.java
+++ front-end/PointStore/app/src/main/java/br/com/pointstore/util/Login.java
@@ -1,31 +1,55 @@
-package br.com.pointstore.util;
+package br.com.pointstore;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.pointstore.R;
 
+
 public class Login extends AppCompatActivity {
+
+    private EditText editTextLogin;
+    private EditText editTextSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        ActionBar bar = getSupportActionBar();
-//        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3CB371")));
-
-
-
         setContentView(R.layout.activity_login);
+
+
+        editTextLogin = (EditText) findViewById(R.id.editTextLogin);
+        editTextSenha = (EditText) findViewById(R.id.editTextSenha);
+
 
     }
 
 
     public void logar(View v) {
 
-    }
+
+        if ((editTextLogin.getText().length() <= 0)) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Login Vazio!", Toast.LENGTH_SHORT);
+            toast.show();
+            editTextLogin.setError("Erro");
+
+        }  if ((editTextSenha.getText().length() <= 0)) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Digite a Senha!", Toast.LENGTH_SHORT);
+            toast.show();
+            editTextSenha.setError("Erro");
+        } if
+            ((editTextLogin.getText().length() <= 0 ) || (editTextSenha.getText().length() <= 0 )) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Campos Vazios", Toast.LENGTH_SHORT);
+                toast.show();
+                editTextSenha.setError("erro");
+            }
+        }
+
+
+
     public void cadastrarUsuario (View v) {
         Intent cadastrarUsuario = new Intent(this, CadastrarUsuario.class);
         startActivity(cadastrarUsuario);
