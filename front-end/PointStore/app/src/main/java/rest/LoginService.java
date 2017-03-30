4f543c84289d3d6576c3851b9bca092ff3e8b751
romulo-xavier25 <romulo.xavier25@gmail.com>
2017-03-30 17:22:06 -0300

    criando o serviço POST para realizar a comunicação com Back-End para poder logar

--- front-end/PointStore/app/src/main/java/rest/LoginService.java
+++ front-end/PointStore/app/src/main/java/rest/LoginService.java
@@ -6,14 +6,18 @@ import br.com.pointstore.model.Usuario;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 /**
- * Created by root on 21/03/17.
+ * Created by Romulo Xavier on 21/03/17.
  */
 
 public interface LoginService {
+
+    @POST("/PointStoreWeb/rest/usuario")
+    Call<Usuario> loginUser(@Body Usuario usuario);
 
     @GET("/PointStoreWeb/rest/usuarioLogin")
     Call<Collection<Usuario>> logar();
